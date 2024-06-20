f82ce164f3a8768f20b916811605d1e8a96571ed
manichodi <chodimani28@gmail.com>
2024-06-20 11:33:33 +0530

    static block

--- TestFirst/src/main/java/com/demo/TestFirst/TestFourth.java
+++ TestFirst/src/main/java/com/demo/TestFirst/TestFourth.java
@@ -1,10 +1,15 @@
 package com.demo.TestFirst;
 
 public class TestFourth {
-	public TestFourth
+	static {
+		int x = 10;
+		int y = 20;
+		int z = 30;
+		System.out.println(z);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
