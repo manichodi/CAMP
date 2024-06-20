18a2b779c62b2b318b74b5e5f61e6d12f0864467
manichodi <chodimani28@gmail.com>
2024-06-20 11:08:06 +0530

    constructor

---
+++ TestFirst/src/main/java/com/demo/TestFirst/TestFourth.java
@@ -0,0 +1,11 @@
+package com.demo.TestFirst;
+
+public class TestFourth {
+	public TestFourth
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestFirst/src/main/java/com/demo/TestFirst/TestThird.java
@@ -0,0 +1,18 @@
+package com.demo.TestFirst;
+
+public class TestThird{
+	public TestThird() {
+		System.out.println("wellcome");
+	}
+	public void hello() {
+		System.out.println("hello");
+	}
+
+	public static void main(String[] args) {
+		TestThird t = new TestThird();
+		t.hello();
+		
+
+	}
+
+}
